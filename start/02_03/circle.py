31aa493761a5a4828f47429524fdaa3e34a32ab2
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2025-08-10 12:09:31 +0000

    Area and circumference of circle

--- start/02_03/circle.py
+++ start/02_03/circle.py
@@ -1,3 +1,13 @@
 """
 A class for representing a circle
 """
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
+    def display_circumference(self):
+        print(f"Circumference:{2*3.14*self.radius}")
+    def display_area(self):
+        print(f"Area:{3.14*self.radius*self.radius}")
+new=Circle(2)
+new.display_circumference()
+new.display_area()
