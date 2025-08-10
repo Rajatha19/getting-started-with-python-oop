52ef93522206d1658430058bf14582dd7b1acc25
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2025-08-10 13:40:12 +0000

    Polymorphism

--- start/06_02/polymorphism_example.py
+++ start/06_02/polymorphism_example.py
@@ -2,5 +2,14 @@
 Python Polymorphism example
 """
 
-
+class ClassA:
+    def meme(self):
+        print("meme() from class A")
+class ClassB(ClassA):
+    def meme(self):
+        print("meme() from class B") 
+a=ClassA()
+b=ClassB()
+a.meme()
+b.meme()
 
