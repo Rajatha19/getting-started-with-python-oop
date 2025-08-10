5d67d09e19862c01bcd9134d237d04a14b8369e2
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2025-08-10 11:48:15 +0000

    First Coding Challenge : Class

--- start/02_01/properties.py
+++ start/02_01/properties.py
@@ -1,3 +1,10 @@
 """
 Python class properties
 """
+class Rectangle:
+    def __init__(self,base,height):
+       self.base=base
+       self.height=height
+new=Rectangle(12,10)
+print(new.base)
+print(new.height)      
